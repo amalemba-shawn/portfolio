dd1f2ed980fb17c11a4557b0fc3c515824fcbf28
Shawn Mulula <shawnamalemba@gmail.com>
2025-10-30 09:34:34 +0300

    Updated hero section

--- data/contactData.ts
+++ data/contactData.ts
@@ -3,6 +3,6 @@ import { ContactInfo } from '../types';
 export const contactInfo: ContactInfo = {
   email: "shawn@gmail.com",
   phone: "+254796850043",
-  linkedin: "https://www.linkedin.com/in/janedoe",
-  github: "https://github.com/janedoe",
+  linkedin: "www.linkedin.com/in/shawn-amalemba",
+  github: "www.linkedin.com/in/shawn-amalemba",
 };
